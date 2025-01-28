9c14c786447c661c81f793cad71038766675e55e
Ramil <zarram89@gmail.com>
2025-01-28 19:30:21 +0500

    Adds PHP Data Objects explanation to index.php

--- index.php
+++ index.php
@@ -1,4 +1,23 @@
 <?php
 
 require 'functions.php';
-require 'router.php';
+// require 'router.php';
+
+//connect to MySQL datavase. PDO
+
+class Person {
+  public $name;
+  public $age;
+
+  public function  breathe() {
+    echo $this->name . ' is breathing';
+  }
+}
+
+$person = new Person();
+$person->name = "John Doe";
+$person->age = 25;
+$person->breathe();
+
+#dsn - data source name (post, host, dataset, charset)
+new PDO($dsn);
