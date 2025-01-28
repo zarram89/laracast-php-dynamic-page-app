ccb0b94d06c2d24313bee0d35de7f16cb7fd6b52
Ramil <zarram89@gmail.com>
2025-01-28 20:10:58 +0500

    Adds Database class to project

---
+++ Database.php
@@ -0,0 +1,16 @@
+<?php
+
+class Database {
+  public $connection;
+  public function __construct() {
+    $dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
+    // Tip: This should be wrapped in a try-catch. We'll learn how, soon.
+    $this->connection = new PDO($dsn);
+  }
+  public function query($query)  {
+    $statement = $this->connection->prepare($query);
+    $statement->execute();
+
+    return $statement;
+  }
+}
--- index.php
+++ index.php
@@ -1,17 +1,12 @@
 <?php
-
-require 'functions.php';
 // require 'router.php';
+require 'functions.php';
 
-$dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
+require 'Database.php';
 
-// Tip: This should be wrapped in a try-catch. We'll learn how, soon.
-$pdo = new PDO($dsn);
+$db = new Database();
 
-$statement = $pdo->prepare("select * from posts");
-$statement->execute();
-
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+$posts = $db->query("SELECT * FROM posts WHERE id = 2")->fetchAll(PDO::FETCH_ASSOC);
 
 foreach ($posts as $post) {
   echo "<li>". $post["title"] ."</li>";
