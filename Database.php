d71c5d4a149d329523f0986ff050ebe0bd68fe7d
Ramil <zarram89@gmail.com>
2025-01-28 20:54:25 +0500

    Adds refactoring to code and congif files

--- Database.php
+++ Database.php
@@ -2,10 +2,13 @@
 
 class Database {
   public $connection;
-  public function __construct() {
-    $dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
+  public function __construct($config, $username = 'root', $password = '') {
+    $dsn = "mysql:" . http_build_query($config, '', ';');
+
     // Tip: This should be wrapped in a try-catch. We'll learn how, soon.
-    $this->connection = new PDO($dsn);
+    $this->connection = new PDO($dsn, $username, $password,[
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
   }
   public function query($query)  {
     $statement = $this->connection->prepare($query);
--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
-// require 'router.php';
-require 'functions.php';
 
+require 'functions.php';
+// require 'router.php';
 require 'Database.php';
 
-$db = new Database();
+$config = require('config.php');
 
+$db = new Database($config['database']);
 $posts = $db->query("SELECT * FROM posts WHERE id = 2")->fetchAll(PDO::FETCH_ASSOC);
 
 foreach ($posts as $post) {
