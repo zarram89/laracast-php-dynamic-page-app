b63020978123d9843e36348429e3e080db3d1f30
Ramil <zarram89@gmail.com>
2025-01-29 23:21:19 +0500

    Adds Response class and some authorisation

--- controllers/note.php
+++ controllers/note.php
@@ -5,8 +5,20 @@ $db = new Database($config['database']);
 
 $heading = 'Note';
 
-$note = $db->query("SELECT * FROM notes WHERE id = :id", ['id' => $_GET['id']])->fetch();
+$note = $db->query("SELECT * FROM notes WHERE id = :id", [
+    "id" => $_GET['id']
+  ])->fetch();
 
 // dd($notes);
+
+if (! $note) {
+  abort();
+}
+
+$currentUserId = 1;
+
+if ($note['user_id'] !== $currentUserId ) {
+  abort(Response::FORBIDDEN);
+}
 
 require "views/note.view.php";
